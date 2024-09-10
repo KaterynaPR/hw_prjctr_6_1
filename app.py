ef530cdaf76d4805956664a55b3bb0514fef6f68
Kate Rul <k0664488906@gmail.com>
2024-09-10 22:12:48 +0300

    add divide fuction to calc

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero!"
+    return x / y
+
 def add(x, y):
     return x + y
 
@@ -6,22 +14,26 @@ def subtract(x, y):
 
 def main():
     while True:
-        choice = input('Input number operation: 1 - you want to add, 2 - to subtract or "q" to quit: ')
+        choice = input('Input number operation: 1 - you want to add, 2 - to subtract, 3 - to multiply, 4 - to divide, or "q" to quit: ')
 
         if choice == 'q':
             print('Goodbye!')
             break
 
-        if choice in ['1', '2']:
+        if choice in ['1', '2', '3', '4']:
             num1 = float(input('Input first number: '))
-            num2 = float(input('Input 2nd number: '))
+            num2 = float(input('Input second number: '))
 
             if choice == '1':
                 print(f'Result: {num1} + {num2} = {add(num1, num2)}')
             elif choice == '2':
                 print(f'Result: {num1} - {num2} = {subtract(num1, num2)}')
+            elif choice == '3':
+                print(f'Result: {num1} * {num2} = {multiply(num1, num2)}')
+            elif choice == '4':
+                print(f'Result: {num1} / {num2} = {divide(num1, num2)}')
         else:
-            print('Not correct. Choose 1(to add) or 2(to subtract) ')
+            print('Not correct. Choose 1(to add), 2(to subtract), 3(to multiply), 4(to divide)')
 
 if __name__ == "__main__":
     main()
